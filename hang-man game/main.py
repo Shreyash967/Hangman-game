39f7ad8152f5b39576ce11b8a981324d64e43d80
Shreyash967 <kashyapshreyash85@gmail.com>
2026-02-26 00:00:34 +0530

    Add files via upload

---
+++ hang-man game/main.py
@@ -0,0 +1,53 @@
+import random
+from hangman_words import word_list
+from hangman_ascii import hangman_logo,stages
+
+def start_game():
+    a = input("Enter 'S'To Start the game:").lower()
+    if a == "s":
+        random_word = random.choice(word_list).lower()
+        placeholder = ""
+        for i in range(len(random_word)):
+            placeholder += "-"
+        print(placeholder)
+        print(stages[6])
+        lives=6
+        correct_guesses =[]
+        game_over = False
+        while not game_over:
+            print(f"You have R.lives: {lives}")
+            user_choice = input("Enter your choice:").lower()
+            if user_choice in correct_guesses:
+                print(f"You have already guessed this word {user_choice}")
+            display = ""
+            for letter in random_word:
+                if user_choice == letter:
+                    display += letter
+                    correct_guesses.append(letter)
+                elif letter in correct_guesses:
+                   display += letter
+                else:
+                    display += "-"
+            print(display)
+
+            if user_choice not in random_word:
+                lives -= 1
+                if lives == 0:
+                    game_over = True
+                    print(f"the Correct Word Was {random_word}...")
+                    print(" ********************** Game Over!!!! ***************************")
+
+
+            if "-" not in  display :
+                game_over = True
+                print(f"You Guessed The Correct Word Was {display}")
+                print("*********************  You Winnnn!!! *******************")
+            print(stages[lives])
+    else:
+        print("Invalid input")
+        start_game()
+
+print("Welcome To Hangman Game : \n Description :- \n This a Popular game of hangman, In this You have 6 lives,\n You have to guess the Words before you get hanged")
+print("There Will Be a Progress Bar and Remaining live Count (R.Lives:) \n That is All, Now Plz enjoy game of hangman.  ")
+print(hangman_logo)
+print(start_game())
