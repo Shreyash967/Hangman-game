b1fcba465bd8022cddde763c44ca012c70c3d2ea
Shreyash967 <kashyapshreyash85@gmail.com>
2026-03-23 11:32:46 +0530

    Remove and re-add word list in hangman_words.py

--- hang-man game/hangman_words.py
+++ hang-man game/hangman_words.py
@@ -14,23 +14,5 @@ word_list = [
     "banner", "barley", "barrel", "barrier", "base", "basic", "basket", "battery",
     "battle", "beach", "beam", "bean",
 
-    # --- Fruits & Vegetables ---
-    "apricot", "artichoke", "asparagus", "blackberry", "blueberry", "broccoli", "cabbage",
-    "cantaloupe", "carrot", "cauliflower", "celery", "cherry", "coconut", "corn",
-    "cucumber", "currant", "daikon", "date", "dragonfruit", "durian", "eggplant",
-    "elderberry", "endive", "fennel", "fig", "garlic", "ginger", "gooseberry",
-    "grape", "grapefruit", "guava", "honeydew", "huckleberry", "jicama", "kale",
-    "kiwi", "kumquat", "leek", "lemon", "lettuce", "lime", "lychee", "mango",
-    "melon", "mushroom", "nectarine", "okra", "onion", "orange", "papaya",
-    "parsnip", "peach", "pear", "peas", "pepper", "persimmon", "pineapple",
-    "plantain", "plum", "pomegranate", "potato", "pumpkin", "radish", "raspberry",
-    "rhubarb", "spinach", "strawberry", "tangerine", "tomato", "turnip", "watermelon", "zucchini",
-
-    # --- Indian States & UTs ---
-    "andhra pradesh", "arunachal pradesh", "assam", "bihar", "chhattisgarh", "goa", "gujarat",
-    "haryana", "himachal pradesh", "jharkhand", "karnataka", "kerala", "madhya pradesh",
-    "maharashtra", "manipur", "meghalaya", "mizoram", "nagaland", "odisha", "punjab",
-    "rajasthan", "sikkim", "tamil nadu", "telangana", "tripura", "uttar pradesh",
-    "uttarakhand", "west bengal", "delhi", "jammu and kashmir", "ladakh", "puducherry",
-    "chandigarh", "andaman and nicobar", "lakshadweep"
+   
 ]
